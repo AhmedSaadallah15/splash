d7533eb8f4bd2008b4b4d568663eaf8aed438f78
AhmedSaadallah1 <Ahmedmohamed19982@yahoo.com>
2025-01-18 00:30:39 +0200

    sec commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from "./components/products/products.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, HomeComponent, ProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
